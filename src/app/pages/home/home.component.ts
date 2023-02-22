9d8f4f7ab231d39a95caea7cdcf1796d5797e3ea
Aum-Andra <andra.aum@gmail.com>
2023-02-22 18:48:50 +0100

    addedSwiper

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,130 @@
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
+import { Title,Meta } from '@angular/platform-browser';
+import { BehaviorSubject } from 'rxjs';
+import SwiperCore, { Keyboard, Pagination, Navigation, Virtual } from 'swiper';
+
+SwiperCore.use([Keyboard, Pagination, Navigation, Virtual]);
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  slides$ = new BehaviorSubject<string[]>(['']);
+
+  constructor(private service: MovieApiServiceService,private title:Title,private meta:Meta) {
+    this.title.setTitle('Home - showtime');
+    this.meta.updateTag({name:'description',content:'watch online movies'});
+
+
+
+
+   }
+
+  bannerResult: any = [];
+  trendingMovieResult: any = [];
+  actionMovieResult: any = [];
+  adventureMovieResult: any = [];
+  animationMovieResult: any = [];
+  comedyMovieResult: any = [];
+  documentaryMovieResult: any = [];
+  sciencefictionMovieResult: any = [];
+  thrillerMovieResult: any = [];
+
+  ngOnInit(): void {
+    this.bannerData();
+    this.trendingData();
+    this.actionMovie();
+    this.adventureMovie();
+    this.comedyMovie();
+    this.animationMovie();
+    this.documentaryMovie();
+    this.sciencefictionMovie();
+    this.thrillerMovie();
+
+    // swiper
+
+    this.slides$.next(
+      Array.from({ length: 600 }).map((el, index) => `Slide ${index + 1}`)
+    );
+
+
+  }
+
+
+  // bannerdata
+  bannerData() {
+    this.service.bannerApiData().subscribe((result) => {
+      console.log(result, 'bannerresult#');
+      this.bannerResult = result.results;
+    });
+  }
+
+  trendingData() {
+    this.service.trendingMovieApiData().subscribe((result) => {
+      console.log(result, 'trendingresult#');
+      this.trendingMovieResult = result.results;
+      // this.trendingMovieResult
+    });
+  }
+
+  // action
+  actionMovie() {
+    this.service.fetchActionMovies().subscribe((result) => {
+      this.actionMovieResult = result.results;
+    });
+  }
+
+
+
+
+  // adventure
+  adventureMovie() {
+    this.service.fetchAdventureMovies().subscribe((result) => {
+      this.adventureMovieResult = result.results;
+    });
+  }
+
+
+  // animation
+  animationMovie() {
+    this.service.fetchAnimationMovies().subscribe((result) => {
+      this.animationMovieResult = result.results;
+    });
+  }
+
+
+  // comedy
+  comedyMovie() {
+    this.service.fetchComedyMovies().subscribe((result) => {
+      this.comedyMovieResult = result.results;
+    });
+  }
+
+  // documentary
+  documentaryMovie() {
+    this.service.fetchDocumentaryMovies().subscribe((result) => {
+      this.documentaryMovieResult = result.results;
+    });
+  }
+
+
+  // science-fiction
+  sciencefictionMovie() {
+    this.service.fetchScienceFictionMovies().subscribe((result) => {
+      this.sciencefictionMovieResult = result.results;
+    });
+  }
+
+  // thriller
+  thrillerMovie() {
+    this.service.fetchThrillerMovies().subscribe((result) => {
+      this.thrillerMovieResult = result.results;
+    });
+  }
+
+}
